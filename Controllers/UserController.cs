47583a372b47e1f3a5ccd3a3ef851bf52680a0c1
CheIby <87913133+CheIby@users.noreply.github.com>
2023-04-13 23:44:42 +0700

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.DTO;
+using server.Entities;
+using System.Net;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly WebappContext WebappContext;
+
+        public UserController(WebappContext WebappContext)
+        {
+            this.WebappContext = WebappContext;
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<List<UserDTO>>> Get()
+        {
+            var List = await WebappContext.Users.Select(
+                s => new UserDTO
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Username = s.Username,
+                    Password = s.Password,
+                    EnrollmentDate = s.EnrollmentDate
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetUserById")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int Id)
+        {
+            UserDTO User = await WebappContext.Users.Select(s => new UserDTO
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                Username = s.Username,
+                Password = s.Password,
+                EnrollmentDate = s.EnrollmentDate
+            }).FirstOrDefaultAsync(s => s.Id == Id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return User;
+            }
+        }
+
+        [HttpPost("InsertUser")]
+        public async Task<HttpStatusCode> InsertUser(UserDTO User)
+        {
+            var entity = new User()
+            {
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+            WebappContext.Users.Add(entity);
+            await WebappContext.SaveChangesAsync();
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateUser")]
+        public async Task<HttpStatusCode> UpdateUser(UserDTO User)
+        {
+            var entity = await WebappContext.Users.FirstOrDefaultAsync(s => s.Id == User.Id);
+            entity.FirstName = User.FirstName;
+            entity.LastName = User.LastName;
+            entity.Username = User.Username;
+            entity.Password = User.Password;
+            entity.EnrollmentDate = User.EnrollmentDate;
+            await WebappContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteUser/{Id}")]
+        public async Task<HttpStatusCode> DeleteUser(int Id)
+        {
+            var entity = new User()
+            {
+                Id = Id
+            };
+            WebappContext.Users.Attach(entity);
+            WebappContext.Users.Remove(entity);
+            await WebappContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
